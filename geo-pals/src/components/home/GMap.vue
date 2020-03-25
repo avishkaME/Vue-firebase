145e6c4f018df7b014d74eed25ca6715a526df59
avishka meddegoda <ameddegoda143@gmail.com>
2020-03-25 20:51:38 +0530

    marker added to the map

--- geo-pals/src/components/home/GMap.vue
+++ geo-pals/src/components/home/GMap.vue
@@ -24,7 +24,26 @@ export default {
                 maxZoom: 15,
                 minZoom: 3,
                 streetViewControl: false
-            })  
+            })
+            
+            db.collection('users').get().then(users => {
+                users.docs.forEach(doc => {
+                    let data = doc.data()
+                    if(data.geolocation){
+                        let marker = new google.maps.Marker({
+                            position: {
+                                lat: data.geolocation.lat,
+                                lng: data.geolocation.lng
+                            },
+                            map
+                        })
+                        //add click listner to the marker
+                        marker.addListener('click', ()=>{
+                            console.log(doc.id)
+                        })
+                    }
+                })
+            })
         }
     },
     mounted(){
