2bc7fc14c2687345cdad3a4ac0c6ad5b782c5568
avishka meddegoda <ameddegoda143@gmail.com>
2020-03-25 12:04:25 +0530

    saving users geolocation to firebase

--- geo-pals/src/components/home/GMap.vue
+++ geo-pals/src/components/home/GMap.vue
@@ -5,6 +5,7 @@
 </template>
 <script>
 import firebase from 'firebase'
+import db from '@/firebase/init'
 
 export default {
     name: 'GMap',
@@ -27,12 +28,31 @@ export default {
         }
     },
     mounted(){
+        //get current user
+        let user = firebase.auth().currentUser
+        // console.log(user)
+
         //get users geo location
         if(navigator.geolocation){
             navigator.geolocation.getCurrentPosition(pos => {
                 this.lat = pos.coords.latitude
                 this.lng = pos.coords.longitude
-                this.renderMap()
+
+                //find the user record and update the geocoords
+                db.collection('users').where('user_id', '==', user.uid).get()
+                .then(snapshot => {
+                    snapshot.forEach((doc) => {
+                        db.collection('users').doc(doc.id).update({
+                            geolocation: {
+                                lat: pos.coords.latitude,
+                                lng: pos.coords.longitude
+                            }
+                        })
+                    })
+                }).then(() => {
+                    this.renderMap()
+                })
+     
             }, (err) => {
                 console.log(err)
                 this.renderMap()
